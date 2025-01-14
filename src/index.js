c071a0df949ea73fd8b702bd974ff7ce6ce44228
Nontokozo <nsdube10@gmail.com>
2025-01-14 11:28:46 +0200

    Added basic search engine behaviour

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert("HELLO")
+function handleSearchSubmit(event){
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#weather-app-city")
+    cityElement.innerHTML = searchInput.value
+}
+
+let searchformElement = document.querySelector("#search-form")
+searchformElement.addEventListener("submit", handleSearchSubmit)
