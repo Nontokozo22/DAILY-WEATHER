f0344b455fb1d05d7671ac38e05379d3778b9c94
Nontokozo <nsdube10@gmail.com>
2025-01-15 16:05:03 +0200

    Injected weather forecast basic functionality

--- src/index.js
+++ src/index.js
@@ -48,9 +48,38 @@ function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
    
-    searchCity(searchInput.value)
+    searchCity(searchInput.value);
 }
+
+
+function displayForecast(){
+    let days = ["Wed", "Thur", "Fri", "Sat", "Mon"]
+let forecastHtml = "";
+
+days.forEach(function (day){
+    forecastHtml = 
+    forecastHtml +
+ `
+ <div class="forecast-day">
+        <div class="forecast-date">${day}</div>
+        <div class="forecast-icon">☁</div>
+        <div class="forecast-temperatures">
+         <div class="forecast-temperature"> 
+         <strong>28°</strong> 
+         </div>
+         <div class="forecast-temperature"> 17°</div>
+         </div>
+         </div>
+         `
+         ; 
+        });
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHtml;
+}
+
 
 let searchformElement = document.querySelector("#search-form")
 searchformElement.addEventListener("submit", handleSearchSubmit)
-
+ 
+displayForecast()
+searchCity("Durban")
